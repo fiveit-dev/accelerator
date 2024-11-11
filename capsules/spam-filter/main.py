4c41745b723a8f9fe40f711514a0797454ee0c6a
Alex Fiorenza <alexfiorenza2012@gmail.com>
2024-11-11 16:51:43 -0300

    Feat: Add retrieval function from bucket s3

--- capsules/spam-filter/main.py
+++ capsules/spam-filter/main.py
@@ -1,10 +1,31 @@
 import argparse
 import boto3
 import mlflow
+from botocore.config import Config
+from botocore import UNSIGNED
+from tqdm import tqdm
+
+ALQUIMIA_S3 = "https://minio-api-minio.apps.alquimiaai.hostmydemo.online"
+ALQUIMIA_BUCKET = "alquimia-capsules"
+DATASET_PATH = "datasets/text-classification/spam-filter"
 
 
-def retrieve_from_s3(bucket):
-    print(f"Retrieving model from S3 bucket: {bucket}")
+def retrieve_from_s3(bucket_to_store):
+    s3 = boto3.client(
+        "s3",
+        endpoint_url=ALQUIMIA_S3,
+        config=Config(signature_version=UNSIGNED),
+    )
+
+    paginator = s3.get_paginator("list_objects_v2")
+    pages = paginator.paginate(Bucket=ALQUIMIA_BUCKET, Prefix=DATASET_PATH)
+    try:
+        for page in tqdm(pages, leave=False, desc="Going through pages"):
+            for obj in tqdm(page["Contents"], leave=False, desc="Loading annotations"):
+                data = s3.get_object(Bucket=ALQUIMIA_BUCKET, Key=obj.get("Key"))
+                content = (data["Body"].read()).decode("utf-8")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def upload_to_mlflow(serving_runtime, model_format, metrics):
@@ -20,7 +41,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Retrieve model from S3 and upload to MLflow."
     )
-    parser.add_argument("bucket", type=str, help="Name of the S3 bucket.")
+    parser.add_argument(
+        "--bucket",
+        type=str,
+        help="Name of the S3 bucket where all datasets are going to be stored",
+    )
     parser.add_argument(
         "--serving_runtime",
         type=str,
