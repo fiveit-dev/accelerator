99e94ccbd2e253f959505fe5c868d0a3041f16b0
Tobias <tobias.nimo@alquimia.ai>
2025-07-04 18:36:50 -0300

    VO MCO - Handle CSV files

--- capsules/vital-oceans/test.py
+++ capsules/vital-oceans/test.py
@@ -2,48 +2,83 @@ import asyncio
 from fastmcp import Client
 from pprint import pprint
 
-client = Client("main.py")
+client = Client("main.py") # http://0.0.0.0:8000/mcp
 
 async def test_tools(coords):
     async with client:
 
         results = []
 
-        print("--- Fishing Data ---")
-        result = await client.call_tool("query_activity_data", {"activity": "fishing", "polygon_coords": coords})
+        print("\n\n--- Shapefile Data ---")
+        result = await client.call_tool("show_available_datasets", {"data_type": "shapefile"})
         pprint(result[0].text)
 
-        print("--- Tourism Data ---")
-        result = await client.call_tool("query_activity_data", {"activity": "tourism", "polygon_coords": coords})
-        pprint(result[0].text)
-        print(type(result[0].text))
-
-        print("--- Region ---")
-        result = await client.call_tool("identify_region", {"polygon_coords": coords})
-        pprint(result[0].text)
-        print(type(result[0].text))
-
-        print("--- Humidity ---")
-        result = await client.call_tool("query_satellite_data", {"variable": "humidity", "polygon_coords": coords, "agg_func": "mean"})
-        pprint(result[0].text)
-        print(type(result[0].text))
-
-        print("--- Temperature ---")
-        result = await client.call_tool("query_satellite_data", {"variable": "temperature", "polygon_coords": coords, "agg_func": "std"})
+        print("- AMP (Mexico)")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "areas_prioritarias/areas_protegidas/mexico/anp-mexico.shp", "polygon_coords": coords})
         pprint(result[0].text)
 
-        print("--- Chlorophylla ---")
-        result = await client.call_tool("query_satellite_data", {"variable": "chlorophylla", "polygon_coords": coords, "agg_func": "median"})
+        print("- AMP (EEUU)")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "areas_prioritarias/areas_protegidas/america_norte/PCA_Baja_to_Bering_2005.shp", "polygon_coords": coords})
         pprint(result[0].text)
 
-        print("--- Geodesic ---")
+        print("- Biodiversidad:")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "areas_prioritarias/zonas_importancia_biodiversidad/zona_importancia_Biodiv_CDB.shp", "polygon_coords": coords})
+        pprint(result[0].text)
+
+        print("- Biología:")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "areas_prioritarias/zonas_importancia_biológica/zona_importancia_biol_pp.shp", "polygon_coords": coords})
+        pprint(result[0].text)
+
+        print("- Refugios Pesqueros:")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "areas_prioritarias/zonas_refugios_pesqueros/zona_refugio_pesquero_COBI2020.shp", "polygon_coords": coords})
+        pprint(result[0].text)
+
+        print("- Turismo:")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "socioeconomico/turismo/INEGI_DENUE_26052025.shp", "polygon_coords": coords})
+        pprint(result[0].text)
+
+        print("- Pesca:")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "socioeconomico/pesca/INEGI_DENUE_26052025.shp", "polygon_coords": coords})
+        pprint(result[0].text)
+
+        print("- Zonas explotación (calamar):")
+        result = await client.call_tool("query_shapefile", {"shapefile_path": "socioeconomico/zonas_explotacion_pesquera/calamar/zona_pesca_calamar.shp", "polygon_coords": coords})
+        pprint(result[0].text)
+
+        print("\n\n--- NetCDF Data ---")
+        result = await client.call_tool("show_available_datasets", {"data_type": "netcdf"})
+        pprint(result[0].text)
+
+        print("- Surface wind speed:")
+        result = await client.call_tool("query_netcdf", {"netcdf_path": "surface_wind_speed.nc", "polygon_coords": coords, "agg_func": "max"})
+        pprint(result[0].text)
+
+        print("- Temperature:")
+        result = await client.call_tool("query_netcdf", {"netcdf_path": "humidity.nc", "polygon_coords": coords, "agg_func": "min"})
+        pprint(result[0].text)
+
+        print("- Temperature:")
+        result = await client.call_tool("query_netcdf", {"netcdf_path": "temperature.nc", "polygon_coords": coords, "agg_func": "series"})
+        pprint(result[0].text)
+
+        print("\n\n--- CSV Data ---")
+        result = await client.call_tool("show_available_datasets", {"data_type": "csv"})
+        pprint(result[0].text)
+
+        print("- Schema:")
+        result = await client.call_tool("get_csv_schema", {"csv_path": "tablas_biodiversidad/peces_bahia_lapaz.csv"})
+        pprint(result[0].text)
+
+        print("- Query:")
+        result = await client.call_tool("query_csv", {"csv_path": "tablas_biodiversidad/peces_bahia_lapaz.csv", "sql_query": "SELECT * FROM peces_bahia_lapaz"})
+        pprint(result[0].text)
+
+        print("\n\n--- Geodesic ---")
         result = await client.call_tool("calculate_geodesic_perimeter", {"polygon_coords": coords})
         print(f"- Perimeter: {result[0].text}")
-        print(type(result[0].text))
         
         result = await client.call_tool("calculate_geodesic_area", {"polygon_coords": coords})
         print(f"- Area: {result[0].text}")
-        print(type(result[0].text))
 
 # Polygon coords from Baja California
 coords = [
