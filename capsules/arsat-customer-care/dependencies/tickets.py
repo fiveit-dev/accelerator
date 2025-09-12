ff39ad1289a4ee2d7a846302b60b93a590ef049a
Francisco Martin <fmartin@fiveit.com.ar>
2025-09-12 15:30:41 +0000

    feat: Refactor ticket query logic in MaximoTicketsProvider; remove create_ticket method

--- capsules/arsat-customer-care/dependencies/tickets.py
+++ capsules/arsat-customer-care/dependencies/tickets.py
@@ -5,14 +5,13 @@ from models.ticket import Ticket
 import httpx
 from loguru import logger
 import json
-import urllib.parse
+import base64
+import xmltodict
+import textwrap
 from config.manager import settings
 
 
 class TicketsProvider(ABC):
-    async def query(self, oslc_where_clause: str) -> List[Ticket]:
-        """Query tickets using an OSLC clause."""
-        ...
 
     @abstractmethod
     async def get_active_tickets(self, customer_id: str) -> List[Ticket]:
@@ -25,114 +24,88 @@ class TicketsProvider(ABC):
     ) -> Optional[Ticket]:
         """Retrieve a single ticket by its ID and customer ID."""
         ...
-    
-    @abstractmethod
-    async def create_ticket(self, ticket: Ticket) -> Ticket:
-        """Create a new ticket in the system."""
-        ...
 
 
 class MaximoTicketsProvider(TicketsProvider):
-    maximo_person_obj = "A_TKWL_IA_RH"
 
     def __init__(
         self, base_url, user_id, passwd, request_timeout=10.0, verify_ssl=True
     ):
-        self.base_url = base_url
-        self.request_timeout = request_timeout
-        self.verify_ssl = verify_ssl
-        ticket_fields = ",".join(
-            f"spi:{fld.lower()}"
-            for fld in Ticket.model_fields
-            if fld.lower() != "worklogs"
-        )
-        self.query_args = [
-            f"oslc.select={ticket_fields},spi:worklog",
-            f"_lid={user_id}",
-            f"_lpwd={passwd}",
-        ]
-
-    async def query(self, oslc_where_clause) -> List[Ticket]:
-        """
-        We take `oslc_where_clause` (a string) and combine it with the base query_args.
-        """
-        ## Hardcode the first 10 results for now in a descending order
-        final_args = self.query_args + [
-            oslc_where_clause,
-            "oslc.orderBy=-spi:statusdate",
-            "oslc.pageSize=10",
-        ]
-        async with httpx.AsyncClient(verify=self.verify_ssl) as client:
-            maximo_url = (
-                f'{self.base_url}/{self.maximo_person_obj}?{"&".join(final_args)}'
+        # Para autenticar en la parte de Headers (Usuario y contraseña en Base 64)
+        token = base64.b64encode(f"{user_id}:{passwd}".encode()).decode()
+        self.base_params = {"lean": "1", "_action": "query"}    
+        
+        self._client = httpx.AsyncClient(
+            base_url = base_url,
+            headers = {"maxauth": token},
+            verify=verify_ssl, 
+            timeout=request_timeout
             )
-            logger.debug(f"Attemping to query tickets with {maximo_url}")
-            response = await client.get(maximo_url, timeout=self.request_timeout)
-            response.raise_for_status()
-            r = response.json()
-            tickets_data = r.get("rdfs:member", [])
-            logger.debug(f"Tickets found: {tickets_data}")
-            return [Ticket.from_maximo_oslc(t) for t in tickets_data]
 
-    async def get_active_tickets(self, customer_id) -> List[Ticket]:
-        encoded_statuses = urllib.parse.quote(
-            json.dumps(settings.MAXIMO_OPEN_TICKET_STATUSES)
-        ).replace("%2C%20", "%2C")
-        where_clause = f"oslc.where=spi:status in {encoded_statuses} and spi:pluspcustomer=%22{customer_id}%22"
-        return await self.query(where_clause)
+    async def __aenter__(self):
+        """Permite el uso con 'async with'."""
+        return self
 
-    async def get_ticket_by_id(self, ticket_id, customer_id) -> Optional[Ticket]:
-        where_clause = f'oslc.where=spi:ticketid="{ticket_id}" and spi:pluspcustomer="{customer_id}"'
-        tickets = await self.query(where_clause)
-        return tickets[0] if tickets else None
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """Cierra el cliente al salir del bloque 'async with'."""
+        await self._client.aclose()    
+        
+    def _parse_tickets_from_response(self, response: httpx.Response) -> List[Ticket]:
+        
+        response.raise_for_status() 
+        
+        data = xmltodict.parse(response.text)
+        tickets_data = (
+            data.get("QueryA_TKWL_IA_RHResponse", {})
+            .get("A_TKWL_IA_RHSet", {})
+            .get("TICKET", [])
+        )
+
+        # Maximo puede devolver un solo ticket como un diccionario en lugar de una lista.
+        if isinstance(tickets_data, dict):
+            tickets_data = [tickets_data]
+            
+        logger.debug(f"Tickets encontrados: {len(tickets_data)}")
+        return [Ticket.from_maximo_oslc(t) for t in tickets_data]
     
-    async def create_ticket(self, ticket: Ticket) -> Ticket:
-        url = f"{self.base_url}/{self.maximo_person_obj}" # Se utiliza esta url para crear tickets??
+    async def get_active_tickets(self, customer_id) -> List[Ticket]:
 
-        payload = {
-            "spi:class": ticket.CLASS,
-            "spi:description": ticket.DESCRIPTION,
-            # Debe ser el mismo customer del context de la sesión?
-            "spi:pluspcustomer": ticket.PLUSPCUSTOMER,
-            "spi:status": ticket.STATUS,
-            # Estos dos campos deben ser generados por el sistema?
-            "spi:statusdate": ticket.STATUSDATE, 
-            "spi:ticketid": ticket.TICKETID,
+        statuses = ",".join(f"'{s}'" for s in settings.MAXIMO_OPEN_TICKET_STATUSES)
+        where_clause = f"(status in ({statuses}) and pluspcustomer='{customer_id}')"
+
+        # prueba de cuerpo XML para la consulta de tickets usando QueryA_TKWL_IA_RH
+        xml_body = textwrap.dedent(f"""
+            <max:QueryA_TKWL_IA_RH xmlns:max="http://www.ibm.com/maximo">
+                <max:A_TKWL_IA_RHQuery operandMode="AND">
+                    <max:WHERE>{where_clause}</max:WHERE>
+                </max:A_TKWL_IA_RHQuery>
+            </max:QueryA_TKWL_IA_RH>
+        """)
+
+        logger.debug(f"Attemping to query tickets with {self.base_url}")
+        
+        response = await self._client.post(
+            "/",
+            headers={"Content-Type": "application/xml"},
+            params=self.base_params,
+            content=xml_body.strip()
+        )
+
+        return self._parse_tickets_from_response(response)
+        
+    async def get_ticket_by_id(self, ticket_id, customer_id) -> Optional[Ticket]:
+        params = {
+            **self.base_params,
+            "ticketid": ticket_id,
+            "pluspcustomer": customer_id,
         }
 
-        if ticket.WORKLOGS and len(ticket.WORKLOGS) > 0:
-            payload["spi:worklog"] = [
-                {
-                    # CreateBy y modifiBy Debe ser el mismo usuario que crea el ticket?
-                    # Las fechas create y modify deben ser generadas por el sistema Maximo?
-                    "spi:createby": wl.CREATEBY,
-                    "spi:createdate": wl.CREATEDATE,
-                    "spi:description": wl.DESCRIPTION,
-                    "spi:logtype": wl.LOGTYPE,
-                    "spi:logdescription": wl.LOGDESCRIPTION,
-                    "spi:modifyby": wl.MODIFYBY,
-                    "spi:modifydate": wl.MODIFYDATE,
-                    "spi:siteid": wl.SITEID,
-                }
-                for wl in ticket.WORKLOGS
-            ]
+        logger.debug(f"Attemping to query tickets with {self.base_url}")
 
-        # Filtramos los valores nulos del payload
-        payload = {k: v for k, v in payload.items() if v is not None}
+        response = await self._client.get("/", params=params)
 
-        async with httpx.AsyncClient(verify=self.verify_ssl) as client:
-            logger.debug(f"Posting new ticket to {url} with payload: {payload}")
-            response = await client.post(
-                url,
-                params=self.query_args, # Incluyen los parámetros de autenticación
-                headers={"Content-Type": "application/json"},
-                json=payload,
-                timeout=self.request_timeout
-            )
-            response.raise_for_status()
-            created_ticket_data = response.json()
-            logger.debug(f"Created ticket response: {created_ticket_data}")
-            return Ticket.from_maximo_oslc(created_ticket_data)
+        tickets = self._parse_tickets_from_response(response)
+        return tickets[0] if tickets else None
 
 
 class MaximoFakeTicketsProvider(TicketsProvider):
@@ -161,57 +134,13 @@ class MaximoFakeTicketsProvider(TicketsProvider):
         if tickets and len(tickets) > 0:
             return tickets[0]
         return None
-    
-    async def create_ticket(self, ticket: Ticket) -> Ticket:
-        new_ticketid = ticket.TICKETID or f"Fake-{int(datetime.now().timestamp() * 1000)}"
-
-        new_ticket = {
-            "rowstamp": f"RS-Fake-{(int(datetime.now().timestamp() * 1000))}",
-            "Attributes": {
-                "CLASS": {"content": ticket.CLASS or "SR"},
-                "DESCRIPTION": {"content": ticket.DESCRIPTION or ""},
-                "PLUSPCUSTOMER": {"content": ticket.PLUSPCUSTOMER or ""},
-                "STATUS": {"content": ticket.STATUS or "NEW"},
-                "STATUSDATE": {"content": ticket.STATUSDATE or datetime.now().isoformat()},
-                "TICKETID": {"content": new_ticketid},
-                "TICKETUID": {"content": len(self.tickets) + 1000, "resourceid": True},
-            },
-            "RelatedMbos": {"WORKLOG": []},
-        }
-
-        if ticket.WORKLOGS:
-            for idx, wl in enumerate(ticket.WORKLOGS, start=1):
-                new_wl = {
-                    "rowstamp": f"RS-Fake-{(int(datetime.now().timestamp() * 1000) + idx)}",
-                    "Attributes": {
-                        "CREATEBY": {"content": wl.CREATEBY or "FAKEUSER"},
-                        "CREATEDATE": {"content": wl.CREATEDATE or datetime.now().isoformat()},
-                        "DESCRIPTION": {"content": wl.DESCRIPTION or ""},
-                        "LOGTYPE": {"content": wl.LOGTYPE or "CLIENTNOTE"},
-                        "LOGDESCRIPTION": {"content": wl.LOGDESCRIPTION or ""},
-                        "MODIFYBY": {"content": wl.MODIFYBY or ""},
-                        "MODIFYDATE": {"content": wl.MODIFYDATE or datetime.now().isoformat()},
-                        "SITEID": {"content": wl.SITEID or "FAKESITE"},
-                        "WORKLOGID": {"content": len(new_ticket["RelatedMbos"]["WORKLOG"]) + 1, "resourceid": True},
-                        "CLIENTVIEWABLE": {"content": True}, 
-                    },
-                }
-                new_ticket["RelatedMbos"]["WORKLOG"].append(new_wl)
-
-        self.tickets.append(new_ticket)
-
-        with open(self.tickets_path, "w") as f:
-            json.dump(self.root, f, indent=2)
-
-        return Ticket.from_maximo_dump(new_ticket)
-
 
 def get_tickets_provider():
     def _get_maximo_provider() -> TicketsProvider:
         if settings.MAXIMO_BASE_URL:
             logger.debug("Using MaximoTicketsProvider with real Maximo instance")
             return MaximoTicketsProvider(
-                settings.MAXIMO_BASE_URL,
+                f"{settings.MAXIMO_BASE_URL}/A_TKWL_IA_RH",
                 settings.MAXIMO_USER_ID,
                 settings.MAXIMO_PASSWD,
                 request_timeout=settings.MAXIMO_REQUEST_TIMEOUT,
